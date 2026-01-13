1048e1a2f0a0bce786fb8c5bb327bee05189010e
HyKNH <lachynguyen70@gmail.com>
2026-01-13 14:02:35 -0800

    consistent

--- lotanthuvien/app/api/books/route.ts
+++ lotanthuvien/app/api/books/route.ts
@@ -1,13 +1,13 @@
-import { createClient } from "@/utils/supabase/client";
-import { NextResponse } from "next/server";
+import { createClient } from '@/utils/supabase/client';
+import { NextResponse } from 'next/server';
 
 const supabase = createClient();
 
 export async function GET() {
   const { data, error } = await supabase
-    .from("books")
-    .select(" id, title, title_han, cat ")
-    .order("title", { ascending: true });
+    .from('books')
+    .select(' id, title, title_han, cat ')
+    .order('title', { ascending: true });
 
   if (error) return NextResponse.json({ error: error.message }, { status: 500 });
 
@@ -18,9 +18,9 @@ export async function POST(req: Request) {
   const { id, title, title_han, cat_id } = await req.json();
 
   const { data, error } = await supabase
-    .from("books")
+    .from('books')
     .insert([{ id, title, title_han, cat: cat_id }])
-    .select(" id, title, title_han, cat ");
+    .select(' id, title, title_han, cat ');
 
   if (error) return NextResponse.json({ error: error.message }, { status: 500 });
 
