fc39ba0548aa1ddb3b5e168ccfb11c2528bd0344
HyKNH <lachynguyen70@gmail.com>
2026-01-12 17:44:17 -0800

    add files

---
+++ lotanthuvien/app/api/books/route.ts
@@ -0,0 +1,27 @@
+import { createClient } from '@/utils/supabase/client';
+import { NextResponse } from 'next/server';
+
+const supabase = createClient();
+
+export async function POST(req: Request) {
+   
+
+    const { id, title, author, pages } = await req.json();
+
+    const { data, error } = await supabase
+        .from('books')
+        .insert([{ id, title, author, pages }])
+        .select();
+
+    if (error) return NextResponse.json({ error: error.message }, { status: 500 });
+
+    return NextResponse.json(data, { status: 201 });
+}
+
+export async function GET() {
+    const { data, error } = await supabase.from('books').select('*');
+
+    if (error) return NextResponse.json({ error: error.message }, { status: 500 });
+
+    return NextResponse.json(data, { status: 200 });
+}
---
+++ lotanthuvien/app/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+interface Book {
+  id: string;
+  title: string;
+}
+
+export default function Home() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      const res = await fetch('/api/books');
+      const data: Book[] = await res.json();
+      setBooks(data);
+      }
+      fetchBooks();
+    }, []);
+
+  return (
+    <div>
+      <main>
+        <h1>Books</h1>
+          <div>
+            <ul className="mt-4 space-y-2">
+                {books.map((book) => (
+                    <li key={book.id} className="border p-2 rounded shadow">
+                        <Link href={`/book/${book.id}`}>
+                            {book.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+          </div>
+      </main>
+    </div>
+  );
+}
